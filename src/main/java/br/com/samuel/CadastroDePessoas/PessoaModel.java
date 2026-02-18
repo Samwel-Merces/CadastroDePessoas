616f8d8dd1e8857cea582da9620bbed240e11be1
Samuel <samuelmerces321@gmail.com>
2026-02-18 18:11:37 -0300

    extencao para db adicionada e pessoa modelcriado

---
+++ src/main/java/br/com/samuel/CadastroDePessoas/PessoaModel.java
@@ -0,0 +1,49 @@
+package br.com.samuel.CadastroDePessoas;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity   //esta anotação transforma uma Classe em uma entidade de Banco de Dados
+@Table(name = "tb cadastro_de_pessoas") //cria tabela junto com o nome dela
+public class PessoaModel {
+
+    @Id
+    long id;
+    String nome;
+    String email;
+    int idade;
+    
+
+
+    public PessoaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    
+}
